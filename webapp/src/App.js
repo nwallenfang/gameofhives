9dfdda14d7ad2c1ca97f41fd0a8b427b6b2514fc
Nils <nils@wallenfang.de>
2018-12-13 15:08:07 +0100

    send onClick event from client to server

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -5,7 +5,6 @@ import openSocket from 'socket.io-client';
 const socket = openSocket('http://localhost:8000');
 
 // [dimRows, dimCols]
-const dim = [3, 3];
 
 
 class Square extends React.Component {
@@ -17,7 +16,8 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button className={"square " + this.props.class} />
+      <button className={"square " + this.props.class}
+              onClick={() => this.props.onClick()} />
     );
   }
 }
@@ -33,22 +33,20 @@ class Board extends React.Component {
        boardData: [
       ]
     }
-    // initialize empty grid
-    for(let i = 0; i < dim[0]; i++) {
-      this.state.boardData.push(Array());
-      for(let j = 0; j < dim[1]; j++) {
-        this.state.boardData[i].push("");
-      }
-  }
+    
   }
   render() { // whole board has to be rendered on every state change
     return (
-      <div>
-        {[...Array(dim[0])].map((_, rowIndex) =>
+      <div id="wrapper" className="verticalContainer">
+        {[...Array(this.state.boardData.length)].map((_, rowIndex) =>
           <div className="board-row" key={rowIndex}>
-            {[...Array(dim[1])].map((_, columnIndex) =>
+            {[...Array(this.state.boardData[rowIndex].length)].map((_, columnIndex) =>
               <Square key={rowIndex * columnIndex + columnIndex} 
-              class={this.state.boardData[rowIndex][columnIndex]}/>
+              class={this.state.boardData[rowIndex][columnIndex]}
+              onClick = {() => {console.log("clicked"); socket.emit("clickEvent", {
+                rowIndex: rowIndex,
+                columnIndex: columnIndex,
+              })}}/>
             )}
           </div>
         )}
