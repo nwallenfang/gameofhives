ab55c01b7e8197e03d647350c13ea673004a3339
nw3530s <nils@wallenfang.de>
2019-01-15 09:00:24 +0100

    slight refactoring

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -12,18 +12,18 @@ const socket = openSocket('http://localhost:8000');
 class Square extends React.Component {
   constructor(props) {
     super(props);
-    this.state ={
+    this.state = {
       class: "",
       isPreselected: false,
-    } 
+    }
   }
   render() {
     return (  // {...} entspricht <script> ... </script>
       <button className={"square " + this.props.class + (this.props.preselected ? " lighterBackground " + this.props.color : " ")}
-              onClick={() => {
-                this.setState((prevState) => { return {...prevState, isPreselected: true};});
-                this.props.onClick();
-              }} />
+        onClick={() => {
+          this.setState((prevState) => { return { ...prevState, isPreselected: true }; });
+          this.props.onClick();
+        }} />
     );
   }
 }
@@ -33,22 +33,24 @@ class Board extends React.Component {
     super(props);
     // server broadcasts the game state in a regular interval
     // set Board state to trigger an automatic render
-    socket.on('dataBroadcast', data => this.setState((previousState) => {return {...previousState, ...data};}));
-    socket.on('playerColor', data => this.setState((previousState) => { return {...previousState, color: data.color};}));
+    socket.on('dataBroadcast', data =>
+      this.setState((previousState) => { return { ...previousState, ...data }; }));
+    socket.on('playerColor', data =>
+      this.setState((previousState) => { return { ...previousState, color: data.color }; }));
     this.state = {
-       boardData: [
+      boardData: [
       ],
       preselectedTile: {
         rowIndex: undefined,
         colIndex: undefined,
       },
-      color: undefined // move to props
-    }  
+      color: undefined // move to props and load color in the beginning
+    }
   }
 
   render() { // whole board has to be rendered on every state change
     let loadingBar;
-    if(this.state.tickLength !== undefined) {
+    if (this.state.tickLength !== undefined) {
       loadingBar = <LoadingBar tickLength={this.state.tickLength} />
     } else { // don't show a loadingBar if the server has not sent tickLength yet
       loadingBar = <div />
@@ -58,31 +60,39 @@ class Board extends React.Component {
         {loadingBar}
         <div className="game">
           <div className="game-board">
-            <div id="wrapper" className="verticalContainer">
-              {[...Array(this.state.boardData.length)].map((_, rowIndex) =>
-                <div className="board-row" key={rowIndex}>
-                  {[...Array(this.state.boardData[rowIndex].length)].map((_, columnIndex) =>
-                    <Square key={rowIndex * columnIndex + columnIndex} 
+            {[...Array(this.state.boardData.length)].map((_, rowIndex) =>
+              <div className="board-row" key={rowIndex}>
+                {[...Array(this.state.boardData[rowIndex].length)].map((_, columnIndex) =>
+                  <Square key={rowIndex * columnIndex + columnIndex}
                     class={this.state.boardData[rowIndex][columnIndex]}
-                    onClick = {() => {
+                    onClick={() => {
+                      // send every click to the server
                       socket.emit("clickEvent", {
                         rowIndex: rowIndex,
                         columnIndex: columnIndex,
                       });
-                      this.setState((oldState) => { return {...oldState, preselectedTile: {
-                        rowIndex: rowIndex, colIndex:columnIndex
-                      }};});
+                      // save coordinates of the latest selection in state in order to render 
+                      // the square differently
+                      this.setState((oldState) => {
+                        return {
+                          ...oldState, preselectedTile: {
+                            rowIndex: rowIndex, colIndex: columnIndex
+                          }
+                        };
+                      });
                     }}
                     preselected={rowIndex === this.state.preselectedTile.rowIndex && columnIndex === this.state.preselectedTile.colIndex}
+                    // at the moment, color property is only used to render the preselected square
+                    // maybe the player's color should be displayed in the title bar in the 
+                    // beginning of the game
                     color={this.state.color}
-                    />
-                  )}
-                </div>
-              )}
-            </div>
-            </div>
+                  />
+                )}
+              </div>
+            )}
+          </div>
         </div>
-        </div>
+      </div>
     );
   }
 }
@@ -94,14 +104,14 @@ class LoadingBar extends Component {
       width: 0,
 
     }
-    socket.on('dataBroadcast', data => {
+    socket.on('dataBroadcast', () => {
       this.setState({
         width: 0
       });
     });
     setInterval(() => {
       this.setState((prevState, props) => {
-        if(prevState.width < 100) {
+        if (prevState.width < 100) {
           return ({
             width: prevState.width + 1
           });
@@ -109,8 +119,8 @@ class LoadingBar extends Component {
           return prevState;
         }
       });
-    }, this.props.tickLength/100);
-    
+    }, this.props.tickLength / 100);
+
   }
 
   render() {
@@ -139,10 +149,10 @@ class App extends Component {
         <ButtonGroup className="titleBar" bsSize="large">
           <Button>Register</Button>
           <Button>Login</Button>
-          <Button onClick = {() => {socket.emit("join");} }>Play</Button>
+          <Button onClick={() => { socket.emit("join"); }}>Play</Button>
         </ButtonGroup>
-        
-        <Board />  
+
+        <Board />
       </div>
     );
   }
