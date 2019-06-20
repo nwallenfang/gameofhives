58db0bd08fe45d3ae340aa4783d9e90a6a605e39
Nils <nils@wallenfang.de>
2019-06-20 17:02:03 +0200

    change websocket for https

--- webapp/src/Socket.js
+++ webapp/src/Socket.js
@@ -1,4 +1,4 @@
 // put in its own file so that the socket can be accessed from other files as a Singleton
 import openSocket from 'socket.io-client';
 // connection to server	
-export const socket = openSocket('http://localhost:8000');
+export const socket = openSocket('https://localhost:8000');
