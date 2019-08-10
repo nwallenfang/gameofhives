67af23b97b7889cffc647f7f2320c1dbbe3a10f1
Nils <nils@wallenfang.de>
2019-08-10 17:14:53 +0200

    align player left and opponent right [1]

--- webapp/src/OpponentInfo.js
+++ webapp/src/OpponentInfo.js
@@ -7,27 +7,37 @@ export class OpponentInfo extends Component {
     render() {
         let playerName, opponentName;
         let playerColor, opponentColor;
-        playerName = (
-            <h2>{this.props.playerName}</h2>
+        let playerScore, opponentScore;
+
+        playerScore = (
+            <h2>{this.props.playerScore}</h2>
         );
+        opponentScore = (
+            <h2>{this.props.opponentScore}</h2>
+        );
+
         playerColor = (
             <span id="colorInfoDiv" className={"playerColorSquare " + this.props.playerColor} ></span>
         );
-        // opponentName =
-        //     this.props.opponentName }
-        //     ;
         opponentColor = (
             <span id="colorInfoDiv" className={"playerColorSquare " + this.props.opponentColor} ></span>
         );
         return (
-            <span>
-                <h2>
-                    {this.props.playerName}
-                    {playerColor}
-                    vs.
-                    {this.props.opponentName}
-                    {opponentColor}
-                </h2>
+            <span >
+                <div id="playerInfo">
+                    <h2>
+                        {this.props.playerName}
+                        {playerColor}
+                        {playerScore}
+                    </h2>
+                </div>
+                <div id="opponentInfo">
+                    <h2>
+                        {this.props.opponentName}
+                        {opponentColor}
+                        {opponentScore}
+                    </h2>
+                </div>
             </span>
         );
 
